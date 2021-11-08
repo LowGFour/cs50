4293694404d47a6e8a8e48ffbce5f91470094f7c
LowGFour <dallas@by-the-lee.net>
2021-11-08 18:41:00 +1000

    Initial version

---
+++ wiki/encyclopedia/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+# we want to map encyclopedia urls to 'wiki' instead of 'encyclopedia'
+app_name= "wiki"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<str:title>", views.entry, name="entry")
+]
