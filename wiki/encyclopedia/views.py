6b5116c8ae8f63fe2a85d632aaed8e31789ed14d
LowGFour <dallas@by-the-lee.net>
2021-11-12 22:36:45 +1000

    Markdown to HTML conversion, Error displayed if entry is not found.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from . import util
-
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -8,7 +8,12 @@ def index(request):
     })
 
 def entry(request, title):
+    md = util.get_entry(title) # retrieve markdown from entry file
+    if md is None:
+        html = f"<h1>Error</h1><p>No entry has been found for the supplied title: {title}</p>"
+    else :
+        html = markdown2.markdown(md) # convert markdown text from file to html
     return render(request, "encyclopedia/entry.html", {
         "title": title,
-        "entryBody": util.get_entry(title)
+        "entryBody": html
     })
